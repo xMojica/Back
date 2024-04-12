5f5f08115b1d1c5d3b4dd4b6579f07c1f4169be4
santiago mojica <samojica08@gmail.com>
2024-04-12 00:45:53 -0500

    Crear usuario

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log(`La aplicación está escuchando en http://localhost:${port}`);
+  console.log(`La aplicación está activa en el puerto: ${port}`);
 });
 
+//Validar usuario y contraseña del usuario
 app.post("/login", async (req, res) => {
   const { usuario, contrasena } = req.body;
 
-  // Consultar la base de datos para verificar el usuario y la contraseña
   try {
     const client = await pool.connect();
     const result = await client.query(
@@ -34,8 +34,6 @@ app.post("/login", async (req, res) => {
       [usuario, contrasena]
     );
     client.release();
-
-    // Si las credenciales son correctas, devuelve un 200 OK
     if (result.rows.length > 0) {
       res.status(200).json(result.rows[0]);
     } else {
@@ -44,5 +42,59 @@ app.post("/login", async (req, res) => {
   } catch (err) {
     console.error("Error al consultar la base de datos", err);
     res.status(500).json({ message: "Error al consultar la base de datos" });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const {
+    nit,
+    primernombre,
+    segundonombre,
+    primerapellido,
+    segundoapellido,
+    fechanacimiento,
+    telefono,
+    email,
+    usuario,
+    contrasena,
+  } = req.body;
+
+  try {
+    const client = await pool.connect();
+
+    const existingUser = await client.query(
+      "SELECT * FROM cliente WHERE usuario = $1",
+      [usuario]
+    );
+
+    if (existingUser.rows.length > 0) {
+      client.release();
+      return res.status(400).json({ message: "El usuario ya existe" });
+    }
+
+    const nuevo = await client.query(
+      "INSERT INTO cliente (nit, primernombre, segundonombre, primerapellido, segundoapellido, fechanacimiento, telefono, email, usuario, contrasena) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
+      [
+        nit,
+        primernombre,
+        segundonombre,
+        primerapellido,
+        segundoapellido,
+        fechanacimiento,
+        telefono,
+        email,
+        usuario,
+        contrasena,
+      ]
+    );
+
+    client.release();
+    res.status(201).json({
+      message: "Usuario creado exitosamente",
+      nuevo: nuevo.rows[0],
+    });
+  } catch (err) {
+    console.error("Error al consultar o insertar en la base de datos", err);
+    res.status(500).json({ message: "Error al insertar en la base de datos" });
   }
 });
